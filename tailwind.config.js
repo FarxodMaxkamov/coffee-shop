565f45db5eda15a81122e744856c5e87f0e9cf63
Farkhod <mahkamov-97@mail.ru>
2022-03-08 18:05:29 +0500

    half ready

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "940px",
+    },
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
